c40a25eda1ab4f9120d2015479a9e05bef3a83af
Joanna Ramalho <joanna-karla@hotmail.com>
2023-05-10 14:01:56 -0300

    Desafio Javascript

--- DesafioJS/atv10.js
+++ DesafioJS/atv10.js
@@ -1,13 +1,16 @@
 document.getElementById("cpf").style.display = "none";
 document.getElementById("cnpj").style.display = "none";
+document.getElementById("data").style.display = "none";
 
 function pessoa1(){
     document.getElementById("cpf").style.display = "block";
     document.getElementById("cnpj").style.display = "none";
+    document.getElementById("data").style.display = "block";
 }
 
 function pessoa2(){
     document.getElementById("cnpj").style.display = "block";
     document.getElementById("cpf").style.display = "none";
+    document.getElementById("data").style.display = "none";
 }
 
